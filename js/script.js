756977735b8eada34bbcb8228b55dc2c094734eb
tj <thinupavani@gmail.com>
2023-06-25 17:11:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        var spinner = document.querySelector(".spinner");
+        var content = document.querySelector(".content");
+
+        spinner.style.display = "none";
+        content.style.display = "block";
+    }, 3000);
+});
+
+
+
+//   clear field
+document.getElementById("clearEmail").addEventListener("click", function () {
+    document.getElementById("email").value = "";
+});
+
+//   hide show password
+var passwordInput = document.getElementById("password");
+var togglePasswordButton = document.getElementById("togglePassword");
+
+togglePasswordButton.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        togglePasswordButton.innerHTML = '<i class="bi bi-eye-slash"></i>';
+    } else {
+        passwordInput.type = "password";
+        togglePasswordButton.innerHTML = '<i class="bi bi-eye"></i>';
+    }
+});
